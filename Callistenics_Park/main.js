fda17c8a1f1050d8df76d2e11b93ad160cd43c8c
DamianArszagiVelHarszagi <162477199+DamianArszagiVelHarszagi@users.noreply.github.com>
2025-12-30 01:40:52 +0100

    eindelijk connection met de database + de front end
    ik had te veel node servers open in vs code en daardoor kon ik het niet updaten met de nieuwe zaken die ik bij de backend toevoegde zoals cors

--- Callistenics_Park/main.js
+++ Callistenics_Park/main.js
@@ -1,8 +1,15 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
-const uri =
-	"mongodb+srv://damian_arszagi:<db_password>@cluster0.zle6rw4.mongodb.net/?appName=Cluster0";
+require("dotenv").config();
 
+const cors = require("cors");
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri, {
 	serverApi: {
 		version: ServerApiVersion.v1,
@@ -10,29 +17,25 @@ const client = new MongoClient(uri, {
 		deprecationErrors: true,
 	},
 });
-const app = express();
-const port = 3000;
 
-app.use(express.json()), app.use(express.urlencoded({ extended: true }));
+let database = "";
 
-app.get("/", async (req, res) => {
-	let message = "";
+async function start() {
 	try {
-		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
-		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
-		console.log(
-			"Pinged your deployment. You successfully connected to MongoDB!"
-		);
-		message = "Hello World";
+		console.log("mongoDB connected");
+		database = client.db("calisthenics");
+
+		app.get("/", (req, res) => res.send("server ok + DB connected"));
+
+		app.get("/api/test", (req, res) => {
+			res.json({ ok: true });
+		});
+		app.listen(port, () => console.log(`http://localhost:${port}`));
 	} catch (error) {
-		console.error("error", error);
-		res.status(500).send("failed connect");
-	} finally {
-		// Ensures that the client will close when you finish/error
-		await client.close();
-		res.send(message);
+		console.error("mongodb niet connected", error.message);
+		process.exit(1);
 	}
-});
-app.listen(port, () => console.log(`Server: http://localhost:${port}`));
+}
+start();
