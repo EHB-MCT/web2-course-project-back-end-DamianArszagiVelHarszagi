d7b6f2f3038b15fed662d85dce47a420c3ac6aff
DamianArszagiVelHarszagi <162477199+DamianArszagiVelHarszagi@users.noreply.github.com>
2026-01-01 13:42:52 +0100

    get parks route toegevoegd dat alle parken neemt zonder filter momenteel

--- Callistenics_Park/main.js
+++ Callistenics_Park/main.js
@@ -19,23 +19,33 @@ const client = new MongoClient(uri, {
 });
 
 let database = "";
+let parksCollection = "";
 
 async function start() {
 	try {
 		await client.connect();
 		await client.db("admin").command({ ping: 1 });
 		console.log("mongoDB connected");
+
 		database = client.db("calisthenics");
+		parksCollection = database.collection("parks");
 
 		app.get("/", (req, res) => res.send("server ok + DB connected"));
 
 		app.get("/api/test", (req, res) => {
 			res.json({ ok: true });
 		});
+		app.get("/api/parks", async (req, res) => {
+			try {
+				const parks = await parksCollection.find().toArray(); //later filter maken
+				res.json(parks);
+			} catch (error) {
+				res.status(500).json({ message: "error loading parks" });
+			}
+		});
 		app.listen(port, () => console.log(`http://localhost:${port}`));
 	} catch (error) {
 		console.error("mongodb niet connected", error.message);
-		process.exit(1);
 	}
 }
 start();
