4a376e362c4322e5a3d6340d952a3ca7217966de
DamianArszagiVelHarszagi <162477199+DamianArszagiVelHarszagi@users.noreply.github.com>
2026-02-22 12:47:56 +0100

    verwijdert function start

--- CallistenicsPark/main.js
+++ CallistenicsPark/main.js
@@ -24,177 +24,159 @@ let database = "";
 let parksCollection = "";
 let reviewsCollection = "";
 
-//connectie met mongoDB
-async function start() {
+try {
+	await client.connect();
+	await client.db("admin").command({ ping: 1 });
+	console.log("mongoDB connected");
+
+	database = client.db("calisthenics");
+	// nieuwe collections aanmaken
+	parksCollection = database.collection("parks");
+	reviewsCollection = database.collection("reviews");
+} catch (error) {
+	console.error("mongodb niet connected", error.message);
+}
+
+// ------------------- GET /api/parks -------------------
+
+app.get("/api/parks", async (req, res) => {
 	try {
-		await client.connect();
-		await client.db("admin").command({ ping: 1 });
-		console.log("mongoDB connected");
+		const parks = await parksCollection.find().toArray();
+		res.json(parks);
+	} catch (error) {
+		res.status(500).json({ message: "error loading parks" });
+	}
+});
 
-		// database "calisthenics"
-		database = client.db("calisthenics");
-		// nieuwe collections aanmaken
-		parksCollection = database.collection("parks");
-		reviewsCollection = database.collection("reviews");
-		/*
-		// kleine check ins als het werkt
-		app.get("/", (req, res) => res.send("server ok + DB connected"));
+// ------------------- GET /api/reviews?parkId=... -------------------
 
-		app.get("/api/test", (req, res) => {
-			res.json({ ok: true });
-		});
-		*/
-		// ------------------- GET /api/parks -------------------
+app.get("/api/reviews", async (req, res) => {
+	const parkId = req.query.parkId;
 
-		// alle parken uit park collection
-		app.get("/api/parks", async (req, res) => {
-			try {
-				const parks = await parksCollection.find().toArray();
-				res.json(parks);
-			} catch (error) {
-				res.status(500).json({ message: "error loading parks" });
-			}
-		});
+	// park moet degelijk bestaan
+	if (!parkId) return res.status(400).send("park missing");
 
-		// ------------------- GET /api/reviews?parkId=... -------------------
+	//parkId correcte Mongo ObjectId string
+	if (!ObjectId.isValid(parkId))
+		return res.status(400).send(`${parkId} form not correct`);
 
-		// alle reviews op basis van id
-		app.get("/api/reviews", async (req, res) => {
-			const parkId = req.query.parkId;
+	// reviews die bij park horen
+	const reviews = await reviewsCollection
+		.find({ parkId: new ObjectId(parkId) })
+		.toArray();
 
-			// park moet degelijk bestaan
-			if (!parkId) return res.status(400).send("park missing");
+	res.json(reviews);
+});
 
-			//parkId correcte Mongo ObjectId string
-			if (!ObjectId.isValid(parkId))
-				return res.status(400).send(`${parkId} form not correct`);
+// ------------------- POST /api/parks -------------------
 
-			// reviews die bij park horen
-			const reviews = await reviewsCollection
-				.find({ parkId: new ObjectId(parkId) })
-				.toArray();
+app.post("/api/parks", async (req, res) => {
+	try {
+		// front end stuurt dit
+		const { name, city, rating, open24_7, equipment } = req.body;
 
-			res.json(reviews);
-		});
+		if (!name || !city) {
+			return res.status(400).json({ message: "name and city required" });
+		}
+		let r = Number(rating) || 0;
 
-		// ------------------- POST /api/parks -------------------
+		// als het al een array is dan ok
+		// als het een string is dan splits op comma en trim
+		let eq = [];
+		if (Array.isArray(equipment)) {
+			eq = equipment;
+		} else if (typeof equipment === "string") {
+			eq = equipment
+				.split(",")
+				.map((x) => x.trim())
+				.filter((x) => x.length > 0);
+		}
 
-		// Maakt een nieuw park aan
-		app.post("/api/parks", async (req, res) => {
-			try {
-				// front end stuurt dit
-				const { name, city, rating, open24_7, equipment } = req.body;
+		// park object dat in Mongo komt
+		const park = {
+			name,
+			city,
+			rating: r,
+			open24_7: Boolean(open24_7),
+			equipment: eq,
+			reviewsCount: 0,
+			createdAt: new Date(),
+		};
+		// invoegen
+		const result = await parksCollection.insertOne(park);
 
-				if (!name || !city) {
-					return res.status(400).json({ message: "name and city required" });
-				}
-				let r = Number(rating) || 0;
-
-				// als het al een array is dan ok
-				// als het een string is dan splits op comma en trim
-				let eq = [];
-				if (Array.isArray(equipment)) {
-					eq = equipment;
-				} else if (typeof equipment === "string") {
-					eq = equipment
-						.split(",")
-						.map((x) => x.trim())
-						.filter((x) => x.length > 0);
-				}
-
-				// park object dat in Mongo komt
-				const park = {
-					name,
-					city,
-					rating: r,
-					open24_7: Boolean(open24_7),
-					equipment: eq,
-					reviewsCount: 0,
-					createdAt: new Date(),
-				};
-				// invoegen
-				const result = await parksCollection.insertOne(park);
-
-				res.status(201).json({
-					message: "Park added",
-					id: result.insertedId,
-					data: park,
-				});
-			} catch (error) {
-				// kleine error gehad daarom debugging op error
-				console.error("POST /api/parks ERROR:", error);
-				res.status(500).json({ message: error.message });
-			}
-		});
-
-		// ------------------- POST /api/reviews -------------------
-
-		// voegt review aan park
-		app.post("/api/reviews", async (req, res) => {
-			try {
-				const { parkId, rating, comment } = req.body;
-
-				if (!parkId)
-					return res.status(400).json({ message: "parkId is required" });
-				if (!ObjectId.isValid(parkId))
-					return res.status(400).json({ message: "form not correct" });
-
-				let r = Number(rating) || 0;
-				if (r < 0) r = 0;
-				if (r > 5) r = 5;
-
-				const parkObjectId = new ObjectId(parkId);
-
-				// check of park wel bestaat
-				const park = await parksCollection.findOne({ _id: parkObjectId });
-				if (!park) return res.status(400).json({ message: "park not found" });
-
-				// review object
-				const review = {
-					parkId: new ObjectId(parkId), // verwijst naar park
-					rating: r,
-					comment: comment || "",
-					createdAt: new Date(),
-				};
-				// invoegen review
-				await reviewsCollection.insertOne(review);
-
-				// update park stats
-				const oldCount = Number(park.reviewsCount) || 0;
-				const oldRating = Number(park.rating) || 0;
-
-				// nieuwe count en nieuwe gemiddelde rating berekenen
-				const newCount = oldCount + 1;
-				const newAvg = (oldRating * oldCount + r) / newCount;
-
-				// update park document in MongoDB
-				await parksCollection.updateOne(
-					{ _id: parkObjectId },
-					{ $set: { rating: newAvg, reviewsCount: newCount } }
-				);
-				// haal het updated park terug op om naar frontend te sturen
-				const updatedPark = await parksCollection.findOne({
-					_id: parkObjectId,
-				});
-				res.status(201).json(updatedPark);
-			} catch (error) {
-				res.status(500).json({ message: "error adding reviews" });
-			}
-		});
-		// start express server pas als database connected is
-		app.listen(port, () => {
-			console.log(`Server running on port ${port}`);
-			// probleempjes gehad bij hoisting daarom debug
-			console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
-
-			console.log(
-				"MONGO_URI starts with:",
-				(process.env.MONGO_URI || "").slice(0, 12)
-			);
+		res.status(201).json({
+			message: "Park added",
+			id: result.insertedId,
+			data: park,
 		});
 	} catch (error) {
-		console.error("mongodb niet connected", error.message);
+		// kleine error gehad daarom debugging op error
+		console.error("POST /api/parks ERROR:", error);
+		res.status(500).json({ message: error.message });
 	}
-}
-// server start
-start();
+});
+
+// ------------------- POST /api/reviews -------------------
+
+app.post("/api/reviews", async (req, res) => {
+	try {
+		const { parkId, rating, comment } = req.body;
+
+		if (!parkId) return res.status(400).json({ message: "parkId is required" });
+		if (!ObjectId.isValid(parkId))
+			return res.status(400).json({ message: "form not correct" });
+
+		let r = Number(rating) || 0;
+		if (r < 0) r = 0;
+		if (r > 5) r = 5;
+
+		const parkObjectId = new ObjectId(parkId);
+
+		// check of park wel bestaat
+		const park = await parksCollection.findOne({ _id: parkObjectId });
+		if (!park) return res.status(400).json({ message: "park not found" });
+
+		// review object
+		const review = {
+			parkId: new ObjectId(parkId),
+			rating: r,
+			comment: comment || "",
+			createdAt: new Date(),
+		};
+		// invoegen review
+		await reviewsCollection.insertOne(review);
+
+		// update park stats
+		const oldCount = Number(park.reviewsCount) || 0;
+		const oldRating = Number(park.rating) || 0;
+
+		// nieuwe count en nieuwe gemiddelde rating berekenen
+		const newCount = oldCount + 1;
+		const newAvg = (oldRating * oldCount + r) / newCount;
+
+		// update park document in MongoDB
+		await parksCollection.updateOne(
+			{ _id: parkObjectId },
+			{ $set: { rating: newAvg, reviewsCount: newCount } },
+		);
+		// haal het updated park terug op om naar frontend te sturen
+		const updatedPark = await parksCollection.findOne({
+			_id: parkObjectId,
+		});
+		res.status(201).json(updatedPark);
+	} catch (error) {
+		res.status(500).json({ message: "error adding reviews" });
+	}
+});
+// start express server pas als database connected is
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+	// probleempjes gehad bij hoisting daarom debug
+	console.log("MONGO_URI exists:", !!process.env.MONGO_URI);
+
+	console.log(
+		"MONGO_URI starts with:",
+		(process.env.MONGO_URI || "").slice(0, 12),
+	);
+});
